bd74b5c3baefa0ceed256af9f243df6fb8781016
amirbtb <76230761+amirbtb@users.noreply.github.com>
2025-07-29 15:24:36 +0100

    feat: local storage destination (0.4.1)
    - added local_storage_destination
    - refactored object storage prefixes

--- src/tests/unit_tests/destination/test_local_storage.py
+++ src/tests/unit_tests/destination/test_local_storage.py
@@ -3,65 +3,58 @@ from unittest.mock import patch
 
 import omegaconf
 import pytest
-
-# from hermes.exceptions import ObjectStorageDestinationError
 from generic_object_storage_destination.exceptions import ObjectStorageDestinationError
+from local_storage_destination.connector import LocalStorageDestination
 
-# from hermes.destinations.object_storage import ObjectStorageDestination
-from s3_destination.connector import S3Destination
-
-from hermes import settings
-
-# TEST_PIPELINE_NAME = "sync_float_rates"
-BUCKET_NAME = "a-bucket"
-SOURCE_NAME = "float_rates"
-TEST_SERVICE_PREFIX = settings.ObjectStorageServicesPrefixes.s3.value
-SOURCE_TABLE_NAME = "dirham_change_rates"
+BUCKET_NAME = "a-local-bucket"
+SOURCE_NAME = "local_files"
+TEST_SERVICE_PREFIX = ""
+SOURCE_TABLE_NAME = "my_documents"
 
 
 @pytest.fixture
-def mock_object_storage_config():
-    """Fixture to provide a mock object storage configuration."""
+def mock_local_storage_config():
+    """Fixture to provide a mock local storage configuration."""
     config_dict = {
-        "name": "landing_zone",
+        "name": "local_landing_zone",
         "config": {
             "bucket": BUCKET_NAME,
             "format": "json",
-            "service": "s3",
+            "service": "local_storage",
         },
     }
     return omegaconf.OmegaConf.create(config_dict)
 
 
-def test_object_storage_destination_init(mock_object_storage_config):
-    """Test the initialization of ObjectStorageDestination."""
-    destination = S3Destination(mock_object_storage_config)
-    assert destination.name == "landing_zone"
+def test_local_storage_destination_init(mock_local_storage_config):
+    """Test the initialization of LocalStorageDestination."""
+    destination = LocalStorageDestination(mock_local_storage_config)
+    assert destination.name == "local_landing_zone"
     assert destination.bucket == BUCKET_NAME
     assert destination.prefix == TEST_SERVICE_PREFIX
     assert destination.format == "json"
     assert destination.data_stage == "raw"
 
 
-def test_get_prefix(mock_object_storage_config):
+def test_get_prefix(mock_local_storage_config):
     """Test the _get_prefix method."""
-    destination = S3Destination(mock_object_storage_config)
+    destination = LocalStorageDestination(mock_local_storage_config)
     assert destination.prefix == TEST_SERVICE_PREFIX
 
 
-def test_get_object_path(mock_object_storage_config):
+def test_get_object_path(mock_local_storage_config):
     """Test the get_object_path method."""
-    destination = S3Destination(mock_object_storage_config)
+    destination = LocalStorageDestination(mock_local_storage_config)
     path = destination.get_object_path(SOURCE_NAME, SOURCE_TABLE_NAME)
     expected_path_start = f"{TEST_SERVICE_PREFIX}/{BUCKET_NAME}/{SOURCE_NAME}/{SOURCE_TABLE_NAME}/{SOURCE_TABLE_NAME}_"
     assert path.startswith(expected_path_start)
 
 
-def test_load(mock_object_storage_config, mock_fsspec_open):
+def test_load(mock_local_storage_config, mock_fsspec_open):
     """Test the load method."""
     mock_fsspec, mock_file_instance = mock_fsspec_open
-    destination = S3Destination(mock_object_storage_config)
-    data = {"key": "value"}
+    destination = LocalStorageDestination(mock_local_storage_config)
+    data = {"doc_id": 123, "content": "This is a test document."}
     destination.load(SOURCE_NAME, SOURCE_TABLE_NAME, data)
 
     # Ensure `fsspec.open` was called at least once
@@ -85,9 +78,9 @@ def test_load(mock_object_storage_config, mock_fsspec_open):
 
 
 @patch("fsspec.open", side_effect=Exception("Filesystem error"))
-def test_load_object_storage_exception(mock_fsspec_open, mock_object_storage_config):
-    """Test that ObjectStorageDestination.load raises an exception on fsspec error."""
-    destination = S3Destination(mock_object_storage_config)
+def test_load_object_storage_exception(mock_fsspec_open, mock_local_storage_config):
+    """Test that LocalStorageDestination.load raises an exception on fsspec error."""
+    destination = LocalStorageDestination(mock_local_storage_config)
     data = {"key": "value"}
     with pytest.raises(ObjectStorageDestinationError):
         destination.load("test_source", "test_table", data)
